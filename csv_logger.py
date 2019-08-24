28a89b6677752deb2bd08db7d209adbdd7937665
Dave Lindley <dlindley@e15group.com>
2019-08-24 19:10:18 -0400

    Multiple Thermocouple Logging

--- csv_logger.py
+++ csv_logger.py
@@ -15,7 +15,7 @@ import csv
 from Phidget22Python.send_twilio import send_twilio
 
 
-log_file = "phidget_log.csv"
+log_file = "phidget_log"
 
 
 def ms_since_epoch():
@@ -45,34 +45,33 @@ def calculate_doneness(readings_per_minute=2):
     with open(log_file, 'r') as f:
         time_temp_list = [tuple(line) for line in csv.reader(f)]
 
+    time_temp_list = [i for i in time_temp_list if i]
     total_doneness = 0
     for record in time_temp_list:
-        total_doneness += (
-            0.00000000000000007048 * ((float(record[0])*9/5 + 32) ** 7.29007299056)
-        ) / readings_per_minute
+        total_doneness += 0.00000000000000007048 * (record[3] ** 7.29007299056) / readings_per_minute
 
     return total_doneness, time_temp_list
 
 
-
-def log_to_csv(self, temperature):
+def log_to_csv(self, temperature, channel_number):
     """
     This function is used as a mixin for the Phidget class - i.e. it will be used inside that class at runtime.
     :param self: parent object
     :param temperature: temp float which gets passed in by parent
     :return:
     """
-    # temperature = temperature * 9 / 5 + 32
-    time = ms_since_epoch()
+    temperature = temperature * 9 / 5 + 32
+    timestamp = datetime.now()
 
-    print(f"temperature {time}---> {temperature}")
-
-    with open(log_file, "a+") as output_file:
-        writer = csv.writer(output_file)
-        writer.writerow([temperature, time])
+    print(f"temperature {timestamp}---> {temperature}")
 
     doneness, time_temp_list = calculate_doneness()
-    print(f'doneness is {doneness}')
+
+    with open(f"{log_file}+'_'+{channel_number}+'.csv", "a+") as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow([len(time_temp_list), channel_number, timestamp, temperature, doneness])
+
+    print(f'doneness is {doneness} for channel {channel_number}')
 
     # to send a text every 10 minutes.
     if len(time_temp_list)%20 == 0:
@@ -83,29 +82,47 @@ def log_to_csv(self, temperature):
             print(e)
 
 
-def configure_phidget(serial=542_616, hub=0, channel=0):
+def configure_phidget(phidget):
     """
     :param serial: Serial Number as int
     :param hub: Is this connected to a hub? 1:Yes, 0:No
     :param channel: Channel 0 is Thermocouple. 1 is Board.
     :return: configured phidget
     """
-    ch = TemperatureSensor()
-    ch.setDeviceSerialNumber(serial)
-    ch.setHubPort(hub)
-    ch.setChannel(channel)
-    ch.setOnAttachHandler(onAttachHandler)
-    return ch
+    phidget_channels = []
+
+    for index, sensor in enumerate(phidget['channels']):
+        ch = TemperatureSensor()
+        ch.setDeviceSerialNumber(phidget['serial'])
+        ch.setHubPort(phidget['hub'])
+        ch.setChannel(phidget['channel'][index])
+        ch.setOnAttachHandler(onAttachHandler)
+        phidget_channels.append((ch,sensor))
+
+    return phidget_channels
 
 
 if __name__ == "__main__":
-    BrisklessPhidget = configure_phidget()
 
-    # set the log to firebase mixin to run on every temp change
-    BrisklessPhidget.setOnTemperatureChangeHandler(log_to_csv)
-    print("logging added")
-    # launch temperature reading from Phidget
-    BrisklessPhidget.openWaitForAttachment(10000)
+    #for configuring information for a single phidget
+    phidget = {
+        'serial':542_616,
+        'hub':0,
+        'channels':[
+            0,1
+        ]
+    }
+
+    all_phidget_channels = configure_phidget()
+
+
+    for channel in all_phidget_channels:
+        phidget_channel, channel_number = channel
+        # set the log to firebase mixin to run on every temp change
+        phidget_channel.setOnTemperatureChangeHandler(log_to_csv, channel_number = channel_number)
+        print(f"logging added for channel:{channel_number}")
+        # launch temperature reading from Phidget
+        phidget_channel.openWaitForAttachment(10000)
 
     while True:
         time.sleep(10)
